1a2ae108a947663753e2090b35e5f568149275fc
Roger D <roger-@users.noreply.github.com>
2022-12-05 12:05:36 -0500

    improvements + config file support
    lots of improvements
    support for config file

--- sysmon.sh
+++ sysmon.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
-# sysmon.sh v0.1
+# sysmon.sh v0.2
 
-TOKEN="TOKEN HERE"
-SERVER="192.168.1.1"
-PORT="8123"
+# Home Assistant configuration defaults (can be overwritten in .config file)
+HASS_TOKEN=""
+HASS_SERVER="homeassistant.local"
+HASS_PORT="8123"
+HASS_MQTT_PREFIX="homeassistant"
 
+# MQTT path to publish states
+MQTT_PUB_PATH="/home/servers"
+MQTT_PUBLISH_PERIOD=15
+
+# name of network interface (e.g. eth0) otherwise first active one will be used
+NETWORK_IFACE=""
+
+# debug levels: 0 = DEBUG, 1 = INFO, 2 = WARN, 3 = ERROR
 DEBUG_LEVEL=1
-PUBLISH_PERIOD=15
-NC_DELAY=1
 
-SENSOR_PATH="/api/states/sensor"
-MQTT_PATH="/api/services/mqtt/publish"
+# how long to wait for the server to respond (or sleep if not supported)
+TIMEOUT_SERVER=1
 
-PUBLISH_DELAY=0
-NC_CMD="nc"
-CPU_COUNT=$(grep -c ^processor /proc/cpuinfo)
+##### Logging functions #####
 
 COLOR_RESET="\033[0m"
 COLOR_RED="\033[31;1m"
 COLOR_GREEN="\033[32;1m"
 COLOR_BLUE="\033[33;1m"
 COLOR_GRAY="\033[37;1m"
-
-##### Logging functions #####
-
-# debug levels:
-# 0 = DEBUG, 1 = INFO, 2 = WARN, 3=ERROR
 
 error() {
     if [ $DEBUG_LEVEL -le 3 ]; then
@@ -71,12 +72,34 @@ print_json() {
     echo -n "{$(print_key_vals "$@")}"
 }
 
-macid()
+deref_path_symlink()
 {
-    echo $(cat /sys/class/net/*/address | head -n 1 | sed 's/://g')
+    echo $(cd "$*" && pwd -P)
 }
 
-temperature_sensors() {
+macid()
+{
+    nic_name=$1
+
+    # use nic if given
+    if ! [ -z "$nic_name" ]; then
+        cat /sys/class/net/$nic_name/address | head -n 1 | sed 's/://g'
+        return 0
+    fi
+
+    for nic_path in /sys/class/net/*; do
+        nic_name=$(basename "$nic_path")
+        is_virtual=$(deref_path_symlink "$nic_path" | grep virtual)
+
+        # skip possibly virtual or inactive devices  &&  
+        if [ ! -z "$is_virtual" ] && [ "$(cat $nic_path/operstate)" = "up" ]; then 
+            cat $nic_path/address | head -n 1 | sed 's/://g'
+            break
+        fi
+    done
+}
+
+temperature_sensor_names() {
     if ! ls /sys/class/thermal/thermal_zone* > /dev/null 2>&1; then
         return
     fi
@@ -87,6 +110,8 @@ temperature_sensors() {
 }
 
 ##### Sensor functions #####
+
+CPU_COUNT=$(grep -c ^processor /proc/cpuinfo)
 
 avg_load() {
     loadavg=$(cat /proc/loadavg)
@@ -175,20 +200,41 @@ host_name() {
 
 ##### Network functions #####
 
-post_json_message() {
+HTTP_HEADER_CTYPE="Content-Type: application/json"
+
+HTTP_PATH_SENSOR="/api/states/sensor"
+HTTP_PATH_MQTT="/api/services/mqtt/publish"
+
+post_json_message_curl() {
+    path="$1"
+    json="$2"
+
+    url="http://${HASS_SERVER}:${HASS_PORT}${path}"
+
+    http_code=$(curl -m $TIMEOUT_SERVER --output /dev/null -s -X POST -w "%{http_code}\\n" -H "$HTTP_HEADER_AUTH" -H "$HTTP_HEADER_CTYPE" -d "$json" "$url")
+
+    if [ "$http_code" != "200" ]; then
+        error "received error:"
+        printf '%b\n' "$http_code"
+    else
+        debug "message receipt confirmed"
+    fi
+}
+
+post_json_message_netcat() {
     path="$1"
     json="$2"
 
     data="POST $path HTTP/1.1\r
-Authorization: Bearer $TOKEN\r
-Content-Type: application/json\r
+$HTTP_HEADER_AUTH\r
+$HTTP_HEADER_CTYPE\r
 Content-Length: ${#json}\r
 \r
 "
     data="${data}${json}"
 
-    result=$(printf '%b' "$data" | eval "$NC_CMD $SERVER $PORT" 2>&1)
-    sleep $PUBLISH_DELAY
+    result=$(printf '%b' "$data" | eval "$NETCAT_CMD $HASS_SERVER $HASS_PORT" 2>&1)
+    sleep $TIMEOUT_SERVER
 
     http_code=$(echo "$result" | awk '/^HTTP/{print $2; exit}')
 
@@ -205,8 +251,16 @@ Content-Length: ${#json}\r
     fi
 }
 
+post_json_message() {
+    if [ $HAS_CURL -eq 1 ]; then
+        post_json_message_curl "$1" "$2"
+    else
+        post_json_message_netcat "$1" "$2"
+    fi
+}
+
 post_sensor_state() {
-    path="$SENSOR_PATH.$1"
+    path="$HTTP_PATH_SENSOR.$1"
     json="$2"
 
     post_json_message "$path" "$json"
@@ -217,14 +271,26 @@ post_mqtt() {
     json_str=$(echo "$2" | sed 's/"/\\"/g')
     msg=$(print_json topic \"$topic\" payload "\"$json_str\"")
 
-    post_json_message "$MQTT_PATH" "$msg"
+    post_json_message "$HTTP_PATH_MQTT" "$msg"
 }
 
 publish_state_loop() {
-    info "Publishing state to topic: $STATE_TOPIC"
+    info "Publishing state to topic $STATE_TOPIC every $MQTT_PUBLISH_PERIOD s"
+
+    time_pub_next=$(date +"%s")
 
     while true
     do
+        time_now=$(date +"%s")
+        time_sleep=$(( $time_pub_next - $time_now ))
+
+        if [ $time_sleep -gt 0 ]; then
+            debug "sleeping for $time_sleep s"
+            sleep $time_sleep
+        fi
+
+        time_pub_next=$(( $time_pub_next + $MQTT_PUBLISH_PERIOD))
+
         data="$(host_name),$(memory_usage),$(avg_load),$(uptime_duration),$(disk_usage),$(wifi_signal)"
 
         # add temperature sensors if available
@@ -236,11 +302,8 @@ publish_state_loop() {
 
         json="{${data}}"
 
-        debug "Sending message"
+        debug "Publishing state message"
         post_mqtt "$STATE_TOPIC" "$json"
-        debug "=> done"
-
-        sleep $(($PUBLISH_PERIOD - $NC_DELAY - $PUBLISH_DELAY))
     done
 }
 
@@ -250,7 +313,7 @@ publish_discovery_sensor() {
     unit_of_measurement="$3"
     device_class="$4"
 
-    CONFIG_TOPIC="homeassistant/sensor/$DEVICE_NAME/$param/config"
+    config_topic="$HASS_MQTT_PREFIX/sensor/$DEVICE_NAME/$param/config"
 
     device_name="System statistics $DEVICE_NAME"
     version="$(uname -a)"
@@ -258,8 +321,9 @@ publish_discovery_sensor() {
 
     msg=$(print_key_vals name "\"$name\"")
     msg=$msg,$(print_key_vals state_topic \"$STATE_TOPIC\")
+    msg=$msg,$(print_key_vals json_attributes_topic \"$STATE_TOPIC\")
     msg=$msg,$(print_key_vals unit_of_measurement \"$unit_of_measurement\")
-    msg=$msg,$(print_key_vals expire_after \"$((5 * $PUBLISH_PERIOD))\")
+    msg=$msg,$(print_key_vals expire_after \"$((5 * $MQTT_PUBLISH_PERIOD))\")
     msg=$msg,$(print_key_vals value_template "\"{{ value_json.${param} }}\"")
     msg=$msg,$(print_key_vals unique_id \"$DEVICE_NAME-$param\")
     msg=$msg,$(print_key_vals device "$device")
@@ -271,8 +335,7 @@ publish_discovery_sensor() {
     msg={$msg}
 
     debug "Sending discovery message for state $param"
-    post_mqtt "$CONFIG_TOPIC" "$msg"
-    debug "=> done"
+    post_mqtt "$config_topic" "$msg"
 }
 
 publish_discovery_all() {
@@ -296,7 +359,7 @@ publish_discovery_all() {
     publish_discovery_sensor disk_free_kb "Disk free" "kB"
     publish_discovery_sensor disk_used_pc "Disk used" "%"
 
-    for sensor_name in $(temperature_sensors); do        
+    for sensor_name in $(temperature_sensor_names); do        
         var_name="temperature_${sensor_name}_C"
         sensor_name=$(echo $sensor_name | sed 's/_temp$//' | sed 's/_/ /')
 
@@ -304,23 +367,71 @@ publish_discovery_all() {
     done
 }
 
-if ! cmd_exists nc; then
-    debug "Using 'netcat' binary"
-    NC_CMD="netcat"
-fi
+setup() {
+    config_file_name="$(basename $0 .sh).config"
+    config_path_full="${PATH_CONFIG}/${config_file_name}"
 
-if $NC_CMD 2>&1 | grep "\-w" > /dev/null; then
-    debug "Using netcat delay of $NC_DELAY"
-    NC_CMD="$NC_CMD -w $NC_DELAY"
+    if [ -f "$config_path_full" ]; then
+        info "Loading config from $config_path_full"
+        . "$config_path_full"
+    fi
+
+    # need to set this here in case token gets loaded from file
+    HTTP_HEADER_AUTH="Authorization: Bearer $HASS_TOKEN"
+
+    if cmd_exists curl; then
+        HAS_CURL=1
+        debug  "Using curl"
+        return 0
+    elif cmd_exists netcat; then
+        HAS_NETCAT=1
+        NETCAT_CMD="netcat"
+        debug "Using netcat"
+    elif cmd_exists nc; then
+        HAS_NETCAT=1
+        NETCAT_CMD="nc"
+        debug "Using nc"
+    else
+        error "Missing curl and netcat/nc, please install one"
+        exit 1
+    fi
+
+    if ( $NETCAT_CMD 2>&1 | grep "\-w" > /dev/null ); then
+        debug "netcat supports delay parameter"
+        NETCAT_CMD="$NETCAT_CMD -w $TIMEOUT_SERVER"
+        TIMEOUT_SERVER=0
+    else
+        warning "netcat does not support delay parameter (-w), messages may not be reliably sent and confirmed"
+        HAS_NETCAT_BASIC=1
+    fi
+}
+
+start() {
+    HAS_CURL=0
+    HAS_NETCAT=0
+    HAS_NETCAT_BASIC=0
+    NETCAT_CMD="netcat"
+
+    DEVICE_NAME="$(macid $NETWORK_IFACE)-$(cat /proc/sys/kernel/hostname)"
+    STATE_TOPIC="$MQTT_PUB_PATH/$DEVICE_NAME/state"
+
+    setup
+    publish_discovery_all
+
+    if [ $HAS_NETCAT_BASIC -eq 1 ]; then
+        info "Discovery messages may not have been delivered, resending"
+        publish_discovery_all
+    fi
+
+    publish_state_loop
+}
+
+# determine where to load config -- first parameter or script directory
+if [ ! -z "$1" ]; then
+    PATH_CONFIG="$1"
 else
-    PUBLISH_DELAY=$NC_DELAY
-    NC_DELAY=0
+    PATH_CONFIG="$(dirname $0)"
 fi
 
-DEVICE_NAME="$(macid)-$(cat /proc/sys/kernel/hostname)"
-STATE_TOPIC="/home/servers/$DEVICE_NAME/state"
-
-publish_discovery_all
-publish_state_loop
-
+start
 
